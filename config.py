684365987143e997261a7fad5568361dc02d1c3b
Nicolas Suzor <nic@suzor.com>
2020-05-08 14:33:26 +1000

    add environment variables

--- config.py
+++ config.py
@@ -1,11 +1,17 @@
 from yaml import load
-from os import getcwd
+from os import getcwd, environ
 
 cfg = None
 
 try:
     with open("config.yml", 'r') as ymlfile:
         cfg = load(ymlfile)
+
+    # set environment variables
+    if 'observatory' in cfg:
+        environ['GOOGLE_APPLICATION_CREDENTIALS'] = json_key_file=cfg['BQ_KEY_FILE']
+        environ['GOOGLE_CLOUD_PROJECT'] = cfg['PROJECT_ID']
+
 except Exception as e:
     cwd = getcwd()
     print(
